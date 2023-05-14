c90e7d85f46fd5dcb86f815dfcc68dbab728bd7b
mikhailshepelev <romanist-mike@yandex.ru>
2023-05-14 11:00:28 +0300

    Create spring boot project

---
+++ src/main/java/com/example/cityapp/CityappApplication.java
@@ -0,0 +1,12 @@
+package com.example.cityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityappApplication.class, args);
+	}
+}
